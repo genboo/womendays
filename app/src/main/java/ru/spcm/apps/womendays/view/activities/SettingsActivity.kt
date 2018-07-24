bfb540077ec1b6d10933f7100380f4a4640b9b71
genboo <gen.masj@gmail.com>
2018-07-24 09:47:43 +0300

    INIT

--- app/src/main/java/ru/spcm/apps/womendays/view/activities/SettingsActivity.kt
+++ app/src/main/java/ru/spcm/apps/womendays/view/activities/SettingsActivity.kt
@@ -2,6 +2,8 @@ package ru.spcm.apps.womendays.view.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.layout_app_bar_main.*
 import ru.spcm.apps.womendays.App
 import ru.spcm.apps.womendays.R
 import ru.spcm.apps.womendays.di.components.AppComponent
@@ -19,6 +21,17 @@ class SettingsActivity : AppCompatActivity() {
             component?.inject(this)
         }
 
+        setSupportActionBar(toolbar)
+        toolbar.title = getString(R.string.menu_settings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(null)
+        supportActionBar?.setHomeButtonEnabled(true)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
